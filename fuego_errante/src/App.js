42e37ebff1617f338da4da641b8c1334e2c62335
Juan C. Espitia <juanc.espitia@MacBook-Air-de-Juan.local>
2025-02-13 20:38:57 -0500

    react devtools

--- fuego_errante/src/App.js
+++ fuego_errante/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Saludar from './components/Saludar';
+import React from 'react';
 
 function App() {
+  const user = {
+    name: 'Daisy Dew',
+    age: 33,
+  }; 
+
+  const saludarFn = (name, age) => {
+    //console.log('Hola ' + name + ' tienes ' + age + ' años'); 
+    //las template strings scriben con comillas invertidas (``).
+    console.log(`Hola ${name} tienes ${age} años`);
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar name='Daisy Dew'/>
+        <Saludar user={user} saludarFn={saludarFn}/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- fuego_errante/src/components/Saludar.js
+++ fuego_errante/src/components/Saludar.js
@@ -1,8 +1,12 @@
 import react from 'react';
+//Las funciones deben empezar con mayúscula (Saludar), las var y const deben empezar con minúscula (saludarFn).
 export default function Saludar(props){
+    const  { user, saludarFn } = props;
+    const { name = 'anonimo', age } = user
+    console.log(props);
     return (
         <div>
-            <h2>Holi {props.name}</h2>
+            <button onClick={() => saludarFn(name, age)}>Saludar</button>
         </div>
-    )
+    );
 }
